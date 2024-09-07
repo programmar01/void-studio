96399820b87f82a98f67581af593e7e427fe3e27
programmar01 <matedolidze232@gmail.com>
2024-09-07 20:20:00 +0400

    commit

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var options = {
-        strings: ["CREATIVE","fda","CREATIVE"], 
-        typeSpeed: 150,
-        backSpeed: 150,
-        loop: true
-    };
+const dynamicText = document.querySelector("h1 span");
+const words = ["CREATIVE", "GAME"];
 
-    try {
-        var typed = new Typed(".title_color", options);
-    } catch (error) {
-        console.error("Error initializing Typed.js:", error); 
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
     }
-});
+}
+
+typeEffect();
